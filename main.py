ac7904b9b10b96cdb5e6eed426c1c15f2b37b448
George-droid21 <gmungai804@gmail.com>
2022-05-18 22:04:53 +0300

    python calc

---
+++ main.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    sum = a + b
+    print(sum)
+    return
+
+
+def mult(a,b):
+    mult = a * b
+    print(mult)
+    return
+
+
+def use(a,b):
+    mult = a / b
+    print(mult)
+    return
+
+
+def spo(a,b):
+    mult = a - b
+    print(mult)
+    return
+
+
+a=int(input("value 1: "))
+b=int(input("value 2 :"))
+spo(a, b)
+
